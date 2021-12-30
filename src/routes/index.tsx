e2bc550621dc62e01d5dfa5b25e169cb4776be44
thereallucas98 <lucas1998david@gmail.com>
2021-12-30 14:43:21 -0300

    (feat): Header Component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from '../components/Header';
 import HatsPage from '../pages/HatsPage';
 import HomePage from '../pages/HomePage';
 import ShopPage from '../pages/ShopPage';
@@ -6,6 +7,7 @@ import ShopPage from '../pages/ShopPage';
 export function AppRoutes() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
